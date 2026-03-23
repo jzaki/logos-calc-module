724391479c350a22f2cbaf5a062995db12e674a2
jzaki <james.zaki@proton.me>
2026-03-23 19:28:27 +0000

    Update external_lib to calc_module

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "External Library Module - Example wrapping an external C library";
+  description = "Calculator module - wraps libcalc C library for Logos";
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
@@ -8,20 +8,20 @@
 
     # Example: External library as a flake input
     # Replace with your actual library source
-    example-lib = {
-      url = "github:example/example-lib";
-      flake = false;  # Non-flake source
-    };
+    # example-lib = {
+    #   url = "github:example/example-lib";
+    #   flake = false;  # Non-flake source
+    # };
   };
 
-  outputs = { self, logos-module-builder, nixpkgs, example-lib, ... }:
+  outputs = { self, logos-module-builder, nixpkgs, ... }:
     logos-module-builder.lib.mkLogosModule {
       src = ./.;
       configFile = ./module.yaml;
       
       # Pass the external library input to the builder
-      externalLibInputs = {
-        example_lib = example-lib;
-      };
+      # externalLibInputs = {
+      #   example_lib = example-lib;
+      # };
     };
 }
---
+++ src/calc_module_interface.h
@@ -0,0 +1,22 @@
+#ifndef CALC_MODULE_INTERFACE_H
+#define CALC_MODULE_INTERFACE_H
+
+#include "interface.h"
+#include <QObject>
+#include <QString>
+
+class CalcModuleInterface : public PluginInterface {
+public:
+  virtual ~CalcModuleInterface() = default;
+
+  Q_INVOKABLE virtual int add(int a, int b) = 0;
+  Q_INVOKABLE virtual int multiply(int a, int b) = 0;
+  Q_INVOKABLE virtual int factorial(int n) = 0;
+  Q_INVOKABLE virtual int fibonacci(int n) = 0;
+  Q_INVOKABLE virtual QString libVersion() = 0;
+};
+
+#define CalcModuleInterface_iid "org.logos.CalcModuleInterface"
+Q_DECLARE_INTERFACE(CalcModuleInterface, CalcModuleInterface_iid)
+
+#endif // CALC_MODULE_INTERFACE_H
