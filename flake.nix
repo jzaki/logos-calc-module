b203b3740d7e784cdf49b2ca9232cd3093b0f0ff
jzaki <james.zaki@proton.me>
2026-04-01 18:44:33 +0100

    Changes from stable tutorial branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Calculator module - wraps libcalc C library for Logos";
 
   inputs = {
-    logos-module-builder.url = "github:logos-co/logos-module-builder";
+    logos-module-builder.url = "github:logos-co/logos-module-builder/b6cf87d30e2995e023496fcfc7f06e8127c6ac5b";
 
     # If your external library is a flake input (source to be built by Nix),
     # add it here and pass it via externalLibInputs below.
